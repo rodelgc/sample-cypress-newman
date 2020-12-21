7c55e73893a6badf1bb5b455da5833ff8e07d8f6
rodel <od.rodel.calasagsag@gmail.com>
2020-12-22 04:02:25 +0800

    corrected ts classes

--- e2e/cypress/models/BankAccount.ts
+++ e2e/cypress/models/BankAccount.ts
@@ -3,9 +3,15 @@ import { Chance } from 'chance';
 const chance = new Chance();
 
 class BankAccount {
-  bankName = chance.company();
-  routingNumber = chance.string({ numeric: true, length: 9 });
-  accountNumber = chance.string({ numeric: true, length: 9 });
+  bankName: string;
+  routingNumber: string;
+  accountNumber: string;
+
+  constructor() {
+    this.bankName = chance.company();
+    this.routingNumber = chance.string({ numeric: true, length: 9 });
+    this.accountNumber = chance.string({ numeric: true, length: 9 });
+  }
 }
 
 export default BankAccount;
--- e2e/cypress/models/User.ts
+++ e2e/cypress/models/User.ts
@@ -3,22 +3,31 @@ import { Chance } from 'chance';
 const chance = new Chance();
 
 class User {
-  firstName = chance.first();
-  lastName = chance.last();
-  username = generateUsername(this.firstName, this.lastName);
-  email = chance.email();
-  phoneNumber = generatePhoneNumber();
-  password = 'T3st!';
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+
+  constructor() {
+    this.firstName = chance.first();
+    this.lastName = chance.last();
+    this.username = generateUsername(this.firstName, this.lastName);
+    this.email = chance.email();
+    this.phoneNumber = generatePhoneNumber();
+    this.password = Cypress.env('GLOBAL_PASSWORD');
+  }
 }
 
-function generateUsername(firstName: string, lastName: string) {
-  const formattedFirst = firstName.replace(/\W/gi, '');
-  const formattedLast = lastName.replace(/\W/gi, '');
+function generateUsername(firstName: string, lastName: string): string {
+  const formattedFirst = firstName.replace(/\W/g, '');
+  const formattedLast = lastName.replace(/\W/g, '');
 
   return `${formattedFirst}_${formattedLast}`;
 }
 
-function generatePhoneNumber() {
+function generatePhoneNumber(): string {
   const phone1 = chance.string({ numeric: true, length: 3 });
   const phone2 = chance.string({ numeric: true, length: 3 });
   const phone3 = chance.string({ numeric: true, length: 4 });
